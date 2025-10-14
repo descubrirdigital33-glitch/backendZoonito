4b372a213f9d0aeaaea711555183b651ba9067c0
descubrirdigital33-glitch <descubrirdigital33@gmail.com>
2025-10-14 11:46:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const app = express();
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-
-// // Rutas
-// const authRoutes = require("./routes/authRoutes");
-// const musicRoutes = require("./routes/musicRoutes");
-// const subscriptionRoutes = require("./routes/subscriptionRoutes");
-// const userRoutes = require("./routes/userRoutes");
-// const listaRoutes = require("./routes/listaRoutes");
-// const cdRoutes = require("./routes/cdRoutes");
-// const patrocinioRoutes = require("./routes/patrocinioRoutes");
-// const eventoRoutes = require("./routes/eventoRoutes");
-// const avisosRoutes = require("./routes/avisosRoutes");
-// const shareRoutes = require('./routes/shareRoutes');
-
-// // Middleware
-// app.use(express.json());
-// app.use(bodyParser.json({ limit: "10mb" }));
-// app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
-
-// const allowedOrigins = [
-//   "http://localhost:3000",           // desarrollo
-//   "https://front-zoonito.vercel.app" // producción
-// ];
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         console.log("❌ Bloqueado por CORS:", origin);
-//         callback(new Error("No permitido por CORS"));
-//       }
-//     },
-//     credentials: true,
-//     methods: "GET,POST,PUT,DELETE,OPTIONS",
-//     allowedHeaders: "Content-Type,Authorization"
-//   })
-// );
-
-// // Rutas
-// app.use("/api/eventos", eventoRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use('/api/share', shareRoutes);
-// app.use("/api/music", musicRoutes);
-// app.use("/api/subscription", subscriptionRoutes);
-// app.use("/api/user", userRoutes);
-// app.use("/api/listas", listaRoutes);
-// app.use("/api/cds", cdRoutes);
-// app.use("/api/eventos/patrocinioRoutes", patrocinioRoutes);
-// app.use("/api/avisoadmin", avisosRoutes);
-
-// // Test
-// app.get("/api/test", (req, res) => {
-//   res.send("¡Vamos bien!");
-// });
-
-// // Conexión a MongoDB
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB conectado"))
-//   .catch((err) => console.error(err));
-
-// // Servidor
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-
-
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -125,17 +48,29 @@ const eventoRoutes = require("./routes/eventoRoutes");
 const avisosRoutes = require("./routes/avisosRoutes");
 const shareRoutes = require('./routes/shareRoutes');
 
-app.use("/api/eventos", eventoRoutes);
-app.use("/api/auth", authRoutes);
-app.use('/api/share', shareRoutes);
+// app.use("/api/eventos", eventoRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use('/api/share', shareRoutes);
+// app.use("/api/music", musicRoutes);
+// app.use("/api/subscription", subscriptionRoutes);
+// app.use("/api/user", userRoutes);
+// app.use("/api/listas", listaRoutes);
+// app.use("/api/cds", cdRoutes);
+// app.use("/api/eventos/patrocinioRoutes", patrocinioRoutes);
+// app.use("/api/avisoadmin", avisosRoutes);
+
+
+
 app.use("/api/music", musicRoutes);
 app.use("/api/subscription", subscriptionRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/listas", listaRoutes);
 app.use("/api/cds", cdRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/eventos", eventoRoutes);
 app.use("/api/eventos/patrocinioRoutes", patrocinioRoutes);
+app.use('/api/share', shareRoutes);
 app.use("/api/avisoadmin", avisosRoutes);
-
 app.get("/api/test", (req, res) => {
   res.send("¡Vamos bien!");
 });
@@ -151,6 +86,7 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
 
 
 
