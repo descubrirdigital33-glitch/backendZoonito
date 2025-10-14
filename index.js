e07f9b637bec6da2a0f366f088b7a6c61495610d
descubrirdigital33-glitch <descubrirdigital33@gmail.com>
2025-10-14 12:15:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,29 +48,29 @@ const eventoRoutes = require("./routes/eventoRoutes");
 const avisosRoutes = require("./routes/avisosRoutes");
 const shareRoutes = require('./routes/shareRoutes');
 
-// app.use("/api/eventos", eventoRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use('/api/share', shareRoutes);
-// app.use("/api/music", musicRoutes);
-// app.use("/api/subscription", subscriptionRoutes);
-// app.use("/api/user", userRoutes);
-// app.use("/api/listas", listaRoutes);
-// app.use("/api/cds", cdRoutes);
-// app.use("/api/eventos/patrocinioRoutes", patrocinioRoutes);
-// app.use("/api/avisoadmin", avisosRoutes);
-
-
-
+app.use("/api/eventos", eventoRoutes);
+app.use("/api/eventos/patrocinioRoutes", patrocinioRoutes);
+app.use("/api/auth", authRoutes);
+app.use('/api/share', shareRoutes);
 app.use("/api/music", musicRoutes);
 app.use("/api/subscription", subscriptionRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/listas", listaRoutes);
 app.use("/api/cds", cdRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/eventos", eventoRoutes);
-app.use("/api/eventos/patrocinioRoutes", patrocinioRoutes);
-app.use('/api/share', shareRoutes);
 app.use("/api/avisoadmin", avisosRoutes);
+
+
+
+// app.use("/api/music", musicRoutes);
+// app.use("/api/subscription", subscriptionRoutes);
+// app.use("/api/user", userRoutes);
+// app.use("/api/listas", listaRoutes);
+// app.use("/api/cds", cdRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/eventos", eventoRoutes);
+// app.use("/api/eventos/patrocinioRoutes", patrocinioRoutes);
+// app.use('/api/share', shareRoutes);
+// app.use("/api/avisoadmin", avisosRoutes);
 app.get("/api/test", (req, res) => {
   res.send("¡Vamos bien!");
 });
@@ -86,6 +86,7 @@ mongoose
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
 
 
 
