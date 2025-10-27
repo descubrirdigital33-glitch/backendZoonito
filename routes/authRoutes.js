f39f2c5a69b83554d430b77f774f6fe350becf1f
descubrirdigital33-glitch <descubrirdigital33@gmail.com>
2025-10-26 21:38:04 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,30 @@
+// const express = require("express");
+// const {
+//   register,
+//   login,
+//   verifyEmail,
+//   resendVerificationCode,
+// } = require("../controllers/authController");
+
+// const router = express.Router();
+
+// router.post("/register", register);
+// router.post("/login", login);
+// router.post("/verify", verifyEmail);
+// router.post("/resend", resendVerificationCode);
+
+// module.exports = router;
+
+
+
 const express = require("express");
 const {
   register,
   login,
   verifyEmail,
   resendVerificationCode,
+  requestPasswordReset,
+  resetPassword,
 } = require("../controllers/authController");
 
 const router = express.Router();
@@ -12,5 +33,7 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/verify", verifyEmail);
 router.post("/resend", resendVerificationCode);
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
