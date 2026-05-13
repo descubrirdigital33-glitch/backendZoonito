457c89c667c307c169d093c6b905125c3a270a7a
descubrirdigital33-glitch <descubrirdigital33@gmail.com>
2026-05-13 17:19:51 -0300

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,35 +1,3 @@
-// const nodemailer = require("nodemailer");
-
-// const sendEmail = async (to, subject, text = "", html = "") => {
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       host: process.env.SMTP_HOST,
-//       port: parseInt(process.env.SMTP_PORT || "587", 10),
-//       secure: false,
-//       auth: {
-//         user: process.env.SMTP_USER,
-//         pass: process.env.SMTP_PASS,
-//       },
-//     });
-
-//     await transporter.sendMail({
-//       from: `"MusicApp" <${process.env.SMTP_USER}>`,
-//       to,
-//       subject,
-//       text,
-//       html, // ahora html está definido
-//     });
-
-//     console.log(`Correo enviado a ${to}`);
-//   } catch (error) {
-//     console.error("Error enviando correo:", error);
-//   }
-// };
-
-// module.exports = sendEmail;
-
-
-
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, text, html) => {  // ⬅️ Agrega 'html' como parámetro
